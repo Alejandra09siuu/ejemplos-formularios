2142fcb621ecf526f8b5a4edf60522b587f6023c
SCPC 518 <alesuarezj2004@gmail.com>
2024-12-10 20:08:53 -0400

    mi commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },  
+  { path: 'acercade', component: AcercadeComponent }, 
+   { path: 'acercade', component: AcercadeComponent },
+];@NgModule({
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
+import { PipesComponent } from './pages/pipes/pipes.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    InicioComponent,
+    AcercadeComponent,
+    PipesComponent,
+    NavbarComponent,
+    FooterComponent,
+    CapitalizadoPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
