9dd376769d7a5f038b277301025ea6bda66475d2
SCPC 518 <alesuarezj2004@gmail.com>
2024-12-10 20:18:54 -0400

    docs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AcercadeComponent } from './pages/acercade/acercade.component';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { PipesComponent } from './pages/pipes/pipes.component';
 const routes: Routes = [
   { path: 'inicio', component: InicioComponent },  
   { path: 'acercade', component: AcercadeComponent }, 
-   { path: 'acercade', component: AcercadeComponent },
+  { path: 'pipes', component: PipesComponent },
 ];@NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
